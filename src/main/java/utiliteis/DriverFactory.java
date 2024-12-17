0875948354b806cd50044c6868de0f64f2ea1abe
mazen.ismail <mazenm.ameer@gmail.com>
2024-12-17 08:27:19 +0200

    driver factory edited for headless mode

--- src/main/java/utiliteis/DriverFactory.java
+++ src/main/java/utiliteis/DriverFactory.java
@@ -2,21 +2,49 @@ package utiliteis;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverFactory {
     static WebDriver driver;
+    public static WebDriver driverSetUp(String browserName, boolean isMaximized, boolean headlessMode )
+    {
+        if(browserName.equalsIgnoreCase("Chrome"))
+        {
+            System.out.println("Now You have A chrome driver");
+            if(headlessMode)
+            {
+                System.out.println("Running in headless mode");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless", "--disable-gpu");
+                driver = new ChromeDriver(options);
+            }
+            else {
+                System.out.println("Running in Not headless mode");
+                driver=new ChromeDriver();
+            }
 
-    public static WebDriver iniatieDriver(String browserName, Boolean maximize) {
-        if (browserName.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
-        } else if (browserName.equalsIgnoreCase("fireFox")) {
-            driver = new FirefoxDriver();
         }
-        if (maximize = true) {
+        else if (browserName.equalsIgnoreCase("Edge"))
+        {
+            System.out.println("Now You have A edge driver");
+            if(headlessMode)
+            {
+                System.out.println("Running in headless mode");
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--headless", "--disable-gpu");
+                driver = new EdgeDriver(options);
+            }
+            else {
+                System.out.println("Running in Not headless mode");
+                driver=new EdgeDriver();
+            }
+        }
+        if (isMaximized)
+        {
             driver.manage().window().maximize();
         }
-
-        return driver;
+        return  driver;
     }
 }
